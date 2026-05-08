9ea67e370448079362f1c1fef64884764c9e53a6
 <anaa_alvarad@hotmail.com>
2026-05-07 21:02:20 -0500

    Clase 6 Primera Parte

--- Modulo2/Unidad_2/03_Clase3_Md2/Clase3/src/main/java/shopSystem/Clase3/repository/ProductoRepository.java
+++ Modulo2/Unidad_2/03_Clase3_Md2/Clase3/src/main/java/shopSystem/Clase3/repository/ProductoRepository.java
@@ -1,15 +1,13 @@
 package shopSystem.Clase3.repository;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import shopSystem.Clase3.model.Producto;
 
 import java.util.List;
 
 @Repository
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
+public interface ProductoRepository extends JpaRepository<Producto, Long>, ProductoRepositoryC {
     List<Producto>findByNombre(String nombre);
     List<Producto>findByStock(Integer stock);
     List<Producto> findByPrecioLessThan(Double precio);
