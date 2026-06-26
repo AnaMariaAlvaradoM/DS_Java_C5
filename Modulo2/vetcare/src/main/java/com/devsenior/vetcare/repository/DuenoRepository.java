dbc392ef9f675baa3b5cc27b053f2b69c4df548d
 <anaa_alvarad@hotmail.com>
2026-06-25 21:37:56 -0500

    Clase 1, Modulo 4, proyecto final

---
+++ Modulo2/vetcare/src/main/java/com/devsenior/vetcare/repository/DuenoRepository.java
@@ -0,0 +1,8 @@
+package com.devsenior.vetcare.repository;
+
+import com.devsenior.vetcare.model.Dueno;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DuenoRepository extends JpaRepository<Dueno, Long> {
+}
+
